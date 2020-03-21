5c6708a797019aa71e14ac39122579b79c35de43
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-03-21 15:35:40 +0800

    圆角和块级使用方法

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld>你好世界</HelloWorld>
+    <h3>默认</h3>
+    <FrostedGlass />
+    <h3>使用插槽</h3>
+    <FrostedGlass>你好世界</FrostedGlass>
+    <h3>inline-block</h3>
+    <FrostedGlass class="block">你好世界</FrostedGlass>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import FrostedGlass from "@/components/FrostedGlass.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    FrostedGlass
   }
 };
 </script>
+
+<style lang="stylus" scoped>
+.block {
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+}
+</style>
