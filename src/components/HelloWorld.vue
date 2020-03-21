27c52bc464eaaec67bb55ba232b5343662877ac8
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-03-21 15:26:21 +0800

    修改磨砂玻璃的实现方式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="__frosted-glass-wrap-box__">
     <div class="window-glass">
+      <span></span>
+    </div>
+    <div class="content">
       <h1>{{ msg }}</h1>
     </div>
   </div>
@@ -12,7 +15,22 @@ import Vue from "vue";
 export default Vue.extend({
   name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
+    display: {
+      type: String,
+      default: "inline-block"
+    },
+    blur: {
+      type: String,
+      default: "36px"
+    },
+    image: {
+      type: String,
+      default: "./assets/bg-1.png"
+    }
+  },
+  computed: {
+    style_wrap_box_() {}
   }
 });
 </script>
@@ -20,32 +38,28 @@ export default Vue.extend({
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
 .__frosted-glass-wrap-box__ {
-  margin: 100px;
-  color: green;
-  display: inline-block;
   overflow: hidden;
   position: relative;
 
   .window-glass {
-    &, &::before {
-      display: inline-block;
-      height: 400px;
-      width: 400px;
-    }
+    filter: blur(36px);
+    position: absolute;
+    background: url('./assets/bg-1.png');
+    background-position: center;
+    background-size: cover;
+    z-index: -1;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+  }
 
-    &::before {
-      content: '';
-      position: absolute;
-      filter: blur(36px);
-      background: url('./assets/bg-1.png');
-      background-position: center;
-      background-size: cover;
-      z-index: -1;
-      left: 0;
-      right: 0;
-      top: 0;
-      bottom: 0;
-    }
+  .content, .window-glass {
+    height: 100%;
+    width: 100%;
+  }
+
+  .content {
   }
 }
 </style>
